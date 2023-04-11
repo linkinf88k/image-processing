e25cf43f826246a4e681620855f129c864cf6a20
linkinf88k <linkinf88k@gmail.com>
2023-04-12 00:16:27 +0700

    first commit

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express, { Request, Response, Application } from "express";
+import router from "./routes";
+
+const app: Application = express();
+
+const port: number = 5000;
+
+app.use("/", router);
+app.get("/api", (req: Request, res: Response) => {
+  res.send("Hello, world!");
+});
+
+app.listen(port, (): void => {
+  console.log(`server started at localhost:${port}`);
+});
+
+export default app;
